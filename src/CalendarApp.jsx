b0dc051a9101f289544e4b4e29e857b2895ce6eb
Tobira123 <tobias.rodriguez.99.99@gmail.com>
2024-04-15 23:50:06 -0600

    Hash router agregado

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { AppRouter } from './router';
 import { store } from './store';
 
@@ -7,9 +7,11 @@ import { store } from './store';
 export const CalendarApp = () => {
   return (
     <Provider store={ store }>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter>
         <AppRouter />
-      </BrowserRouter>
+        </HashRouter>
+      {/* <BrowserRouter> */}
     </Provider>
   )
 }
